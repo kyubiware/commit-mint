3a718d06307639087b4eef230c92ddb79203c02a
kyubiware <kyubiware@users.noreply.github.com>
2026-06-06 13:33:43 -0700

    chore(vitest): add coverage thresholds to config

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,12 @@ export default defineConfig({
 		coverage: {
 			provider: "v8",
 			reporter: ["text", "html", "json"],
+			thresholds: {
+				lines: 50,
+				branches: 70,
+				functions: 55,
+				statements: 50,
+			},
 		},
 	},
 });
